27a9573301bc10ce1121b7af49fe3229ad72adeb
Shane T. Geary <shane.geary@glowstik.com>
2025-11-19 09:19:43 -0700

    Refactored fetch function for Home page, pulling data for second featured game section details

--- JS/dataFetch.js
+++ JS/dataFetch.js
@@ -1,51 +1,87 @@
 let rimworldData
+let madGamesTycoonData
 
-async function fetchGame() {
+async function fetchJSON(filepath) {
 	try {
-		const response = await fetch('../Data/rimworld.json')
-		if (!response.ok) {
-			throw new Error(`Response status: ${response.status}`)
-		}
-		const result = await response.json()
-		console.log(result);
-		rimworldData = result
-		renderHeroImage()
+		const [rimworld, madGamesTycoon] = await Promise.all([
+			fetch('../Data/rimworld.json')
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`Response status: ${response.status}`)
+				}
+				return response.json()
+			}),
+			fetch('../Data/madgamestycoon.json')
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`Response status: ${response.status}`)
+				}
+				return response.json()
+			})
+		])
+		
+		console.log(rimworld, madGamesTycoon);
+		rimworldData = rimworld
+		madGamesTycoonData = madGamesTycoon
+		// renderHeroImage()
 		renderFeaturedData()
 	} catch (error) {
 		console.error(error.message);
 	}
 }
 
-function renderHeroImage() {
-	const heroImage = document.getElementById('rimworldCoverArtImg')
-	heroImage.src = rimworldData.background_image
-	console.log(heroImage);
-}
+// function renderHeroImage() {
+// 	const heroImage = document.getElementById('rimworldCoverArtImg')
+// 	heroImage.src = rimworldData.background_image
+// 	console.log(heroImage);
+// }
 
 function renderFeaturedData() {
+	// Hero One
 	const rimworldTitle = document.getElementById('rimworldTitleTag')
 	// const heroOneDescWrapper = document.getElementById('heroOneDescWrapper')
 	const heroOneGenresListTag = document.getElementById('heroOneGenresListTag')
 	const heroOnePlatformListTag = document.getElementById('heroOnePlatformListTag')
 	const heroOneDevTag = document.getElementById('heroOneDevTag')
+	//Hero Two
+	const madGamesTitle = document.getElementById('madGamesTitleTag')
+	const heroTwoGenresListTag = document.getElementById('heroTwoGenresListTag')
+	const heroTwoPlatformListTag = document.getElementById('heroTwoPlatformListTag')
+	const heroTwoDevTag = document.getElementById('heroTwoDevTag')
 
 	// Title
 	rimworldTitle.innerHTML = rimworldData.name_original
+	madGamesTitle.innerHTML = madGamesTycoonData.name_original
 	// heroOneDescWrapper.style.backgroundColor = `#${rimworldData.dominant_color}`
 	// heroOneDesc.innerHTML = rimworldData.description
 	
 	// Genres
 	for(i = 0; i < rimworldData.genres.length; i++) {
+		// Hero One
 		const listElement = document.createElement('li')
 		listElement.innerHTML = rimworldData.genres[i].name
 		heroOneGenresListTag.appendChild(listElement)
 	}
+	for(i = 0; i < madGamesTycoonData.genres.length; i++) {
+		// Hero Two
+		const listElement = document.createElement('li')
+		listElement.innerHTML = madGamesTycoonData.genres[i].name
+		heroTwoGenresListTag.appendChild(listElement)
+	}
 	// Platforms
 	for(i = 0; i < rimworldData.platforms.length; i++) {
+		// Hero One
 		const listElement = document.createElement('li')
 		listElement.innerHTML = rimworldData.platforms[i].platform.name
 		heroOnePlatformListTag.appendChild(listElement)
 	}
+	for(i = 0; i < madGamesTycoonData.platforms.length; i++) {
+		// Hero Two
+		const listElement = document.createElement('li')
+		listElement.innerHTML = madGamesTycoonData.platforms[i].platform.name
+		heroTwoPlatformListTag.appendChild(listElement)
+	}
 	// Developer
 	heroOneDevTag.innerHTML = rimworldData.developers[0].name
+	heroTwoDevTag.innerHTML = madGamesTycoonData.developers[0].name
 }
